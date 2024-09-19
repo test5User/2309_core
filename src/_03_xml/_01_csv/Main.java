5335774fdfc755a34ae4d4cb2a8225899d7d97f3
Test User <Test5User@yandex.by>
2024-09-19 21:33:28 +0300

    Xml

---
+++ src/_03_xml/_01_csv/Main.java
@@ -0,0 +1,9 @@
+package _03_xml._01_csv;
+
+import static _03_xml._01_csv.utils.CatUtils.*;
+
+public class Main {
+    public static void main(String[] args) {
+        printCats(extractFromFle());
+    }
+}
---
+++ src/_03_xml/_01_csv/model/CsvCat.java
@@ -0,0 +1,14 @@
+package _03_xml._01_csv.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class CsvCat {
+    private int number;
+    private String name;
+    private int age;
+}
